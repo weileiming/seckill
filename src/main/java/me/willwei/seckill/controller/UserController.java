59fdce09a36141309a917def24f8981c81c79f45
leiming <2008weileiming@gmail.com>
2019-01-18 15:15:59 +0800

    Add opt method

--- src/main/java/me/willwei/seckill/controller/UserController.java
+++ src/main/java/me/willwei/seckill/controller/UserController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 /**
  * UserController
  *
@@ -25,6 +28,33 @@ public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    /**
+     * 用户获取otp短信接口
+     *
+     * @param telephone
+     * @return
+     */
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telephone") String telephone) {
+        // 需要按照一定的规则生成OTP验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        // 将OTP验证码同对应用户的手机号关联，使用httpsession的方式绑定他的手机号与OTPCODE
+        httpServletRequest.getSession().setAttribute(telephone, otpCode);
+
+        // 将OTP验证码通过短信通道发送给用户，省略
+        System.out.println("telephone = " + telephone + " & otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
